150eea84d42a44bcc8cecd6fa2ddcf1a6e485b58
Nadimpallisathvika <nadimpallisathwika4@gmail.com>
2025-12-19 15:03:33 +0530

    Add gunicorn and fix django production settings

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9stvv8i!dkljxbgn=450p9-7v+1w2hdvuk6!2%-i82tzqw)^4h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
